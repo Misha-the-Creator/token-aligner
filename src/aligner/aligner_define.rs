8c6937b643731bae2aff99a799adece5a9f725d3
Mikhail Zuev <mikhailneue@gmail.com>
2026-06-27 02:28:55 +0300

    create aligner struct

--- src/aligner/aligner_define.rs
+++ src/aligner/aligner_define.rs
@@ -1,15 +1,24 @@
-use tokenizers::{Error, tokenizer::Tokenizer};
 use std::collections::HashMap;
+use tokenizers::tokenizer::Tokenizer;
 
-struct Aligner {
-    pub tokenizer: Tokenizer,
-    pub vocab: HashMap<String, u32>,
-    pub exact_matrix: HashMap<Vec<String>, Vec<String>>
+pub struct Aligner {
+    pub tokenizer: Option<Tokenizer>,
+    pub vocab: Option<HashMap<String, u32>>,
 }
 
 impl Aligner {
-    pub fn create_tokenizer(&self, model_name: &str) -> Result<Tokenizer, Error> {
-        let x = Tokenizer::from_pretrained("deepseek-ai/DeepSeek-V4-Pro", None)?;
-        Ok(x)
+    pub fn create_tokenizer(&mut self, model_name: &str) {
+        let tokenizer = Tokenizer::from_pretrained(model_name, None)
+            .expect("Something goes wrong while getting tokenizer");
+        self.tokenizer = Some(tokenizer);
+    }
+
+    pub fn create_vocab(&mut self) {
+        let vocab = self
+            .tokenizer
+            .as_ref()
+            .expect("Smth goes wrong")
+            .get_vocab(true);
+        self.vocab = Some(vocab);
     }
 }
